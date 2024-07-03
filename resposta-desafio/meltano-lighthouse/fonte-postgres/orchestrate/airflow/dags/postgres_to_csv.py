326731949b70831f14b921fd85c8e1350e21198b
Gabriela Pereira Barros <gabrielapbarros15@gmail.com>
2024-07-03 18:15:27 -0300

    Adicionando pastas

---
+++ resposta-desafio/meltano-lighthouse/fonte-postgres/orchestrate/airflow/dags/postgres_to_csv.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+with DAG(
+    dag_id='postgres_to_csv',
+    schedule_interval="* * * * 1-5",
+    start_date=datetime(2023, 4, 1),
+    catchup=False,
+) as dag:
+
+    run_meltano_task = BashOperator(
+        task_id='run_meltano_task',
+        bash_command='meltano run tap-postgres target-csv',
+    )
